40b4e0720c1e65504cd5659efdaed4ae63798b51
Dat Bong <Datbong2801@>
2021-07-08 16:19:11 +0700

    transaction up

--- src/main/java/com/example/transaction/repository/TransactionRepository.java
+++ src/main/java/com/example/transaction/repository/TransactionRepository.java
@@ -18,35 +18,19 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
 //            "AND t.status = 'SUCCESS'" +
 //            "AND (t.tran_Date  BETWEEN '2021-06-01 00:00:00' AND '2021-06-30 13:45:16')", nativeQuery = true)
 //    Integer getSumTransaction();
-
-    @Query(value = "select count(*) / (select count(*)" +
-            "from transaction_table tt where tt.status = 'SUCCESS'" +
-            "AND tt.module ='FUNDTRANSFER'" +
-            "AND tt.tran_Type='FT_INT_TRANS_ACC'" +
-            "AND (tt.tran_Date BETWEEN '2021-06-01 00:00:00' AND '2021-06-30 15:00:00')" +
-            ")" +
-            "from transaction_table t where t.status = 'FAIL'" +
-            "AND t.module ='FUNDTRANSFER'" +
-            "AND t.tran_Type='FT_INT_TRANS_ACC'" +
-            "AND (t.tran_Date BETWEEN '2021-06-01 00:00:00' AND '2021-06-30 15:00:00')", nativeQuery = true)
-    Integer getTransactionRatio();
-
 //    @Query(value = "select count(t.CIF)" +
 //            "from transaction_table as t where t.module = 'BILLPAY'" +
 //            "AND t.tran_Type = 'PHONE_TOPUP' AND t.status = 'FAIL'" +
 //            "AND (t.tran_Date BETWEEN '2021-01-01 00:00:00' AND '2021-06-30 15:00:00')", nativeQuery = true)
 //    Integer getSumCIF();
 
-    @Query(value = "select sum(t.amount)" +
-            "from transaction_table as t where t.module = 'SAVING'" +
-            "AND t.tran_Type = 'SAVING' AND t.status = 'SUCCESS'" +
-            "AND (t.tran_Date BETWEEN '2021-01-01 00:00:00' AND '2021-06-30 15:00:00')", nativeQuery = true)
+    @Query(value = "call getRatio()", nativeQuery = true)
+    Integer getTransactionRatio();
+
+    @Query(value = "call getSumSaving()", nativeQuery = true)
     Integer getSumSaving();
 
-    @Query(value = "select  t.cif,sum(t.amount) as sumAmount from transaction_table as t where t.module = 'BILLPAY'" +
-            "AND t.tran_Type = 'PHONE_TOPUP' AND t.status = 'SUCCESS'" +
-            "AND (t.tran_Date  BETWEEN '2021-06-01 00:00:00' AND '2021-06-30 13:45:16')" +
-            "group by t.cif order by sumAmount DESC LIMIT 5"
+    @Query(value = "call getTop()"
             , nativeQuery = true)
     List<CustomerSumAmount> getTop5();
 
